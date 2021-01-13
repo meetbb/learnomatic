df33dafca3367938ad3edfc72455ff55eaa3d3a5
meetbrahmbhatt91 <meetbrahmbhatt91@gmail.com>
2021-01-13 17:09:14 +0530

    Initial Commit

---
+++ Learnomatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learnomatic
+//
+//  Created by Meet Brahmbhatt on 13/01/21.
+//  Copyright © 2021 Logiciél. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
