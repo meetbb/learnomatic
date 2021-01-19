0a5cf22389c105e4bf70f16628b77aaafa07f795
meetbrahmbhatt91 <meetbrahmbhatt91@gmail.com>
2021-01-19 22:39:40 +0530

    New code added to authenticate and enter the user into the application.

--- Learnomatic/Login/SignUpViewController.swift
+++ Learnomatic/Login/SignUpViewController.swift
@@ -12,8 +12,13 @@ import Firebase
 
 class SignUpViewController: UIViewController {
     
+    // MARK: Outlets
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var retypePasswordTextField: UITextField!
+    @IBOutlet weak var loadingImage: UIActivityIndicatorView!
     
     
     override func viewDidLoad() {
@@ -23,5 +28,21 @@ class SignUpViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         firstNameTextField.addUnderLine()
         lastNameTextField.addUnderLine()
+        emailAddressTextField.addUnderLine()
+        passwordTextField.addUnderLine()
+        retypePasswordTextField.addUnderLine()
+    }
+    
+    
+    @IBAction func onRegisterBtnClick(_ sender: UIButton) {
+        guard let email = emailAddressTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        self.loadingImage.startAnimating()
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            self.loadingImage.stopAnimating()
+            if (error == nil) {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
 }
